d9ae15d5deb5d48530f97f3ddc0931dd82d46584
Patrick Ly <patrickkimly@gmail.com>
2019-07-24 22:01:42 -0700

    Signup / Login page shown

--- mentor-me/src/App.js
+++ mentor-me/src/App.js
@@ -6,8 +6,8 @@ import { Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage'
 import Profile from './components/Profile/Profile'
 import Conversations from './components/Converstations/Conversations'
-import Login from './components/Login/Login'
-import Signup from './components/SignUp/Signup'
+//import Login from './components/Login/Login'
+//import Signup from './components/SignUp/Signup'
 import PrivateRoute from './components/PrivateRoute/PrivateRoute'
 
 // import { getUsers } from './actions'
@@ -25,18 +25,18 @@ class App extends React.Component {
           exact
           component={LandingPage}
         />
-        <Route
+        {/* <Route
           path='/login'
           render={props => (
             <Login {...props} />
           )}
-        />
-        <Route
+        /> */}
+        {/* <Route
           path='/signup'
           render={props => (
             <Signup {...props} />
           )}
-        />
+        /> */}
         <PrivateRoute
           path="/protected"
           component={Profile}
